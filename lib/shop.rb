e6372f9d2ff625d2975426bfb0d97e6d529ecd61
Michael Linford <linford.mike@gmail.com>
2020-12-16 17:50:38 +0000

    further refactoring

--- lib/shop.rb
+++ lib/shop.rb
@@ -32,14 +32,25 @@ class Shop
     return -1 unless items.is_a? String
 
     items_arr = items.split("")
+    return -1 if calc_total(items_arr) == -1
+    
+    savings
+    @total
+  end
+
+  private
+
+  def calc_total(items_arr)
     items_arr.each { |item|
       return -1 unless @prices.has_key?(item)
       @purchased[item] += 1
       @total += @prices[item]
     }
+  end
+
+  def savings
     @purchased.each_pair { |item, quantity|
       @total -= (quantity / @threshold[item]).floor * @savings[item]
     }
-    @total
   end
 end
